e81e0e8dc275f4793dcf1db8b07b84a515cb89e2
Lucas Maritan Whitaker <lucasmaritanlab@gmail.com>
2021-07-26 15:33:07 -0300

    Forca
    ++90% Lógica
    ++ Bootstrap (MAIN)

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
-function chosenWord() {
+const oJogo = document.getElementById('comecaOJogo')
+const jogo = document.getElementById('jogo')
+const palavra = document.getElementById('palavraV');
+const letra = document.getElementById('letraV');
+let fimDeJogo = false;
+let contador;
+let errouFinal = 0;
 
-    const palavra = document.getElementById('chosenWordInput').value;
-    let wordIs = palavra.split('');
-    console.log(wordIs);
 
-};
+
+oJogo.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    let palavraEsc = document.getElementById('palavraE').value;
+    let arrPalavra = palavraEsc.split('');
+    document.getElementById('palavraE').value = '';
+
+    for (let i = 0; i < arrPalavra.length; i++) {
+        contador = document.createElement('input');
+        contador.setAttribute('type', 'buttom');
+        contador.setAttribute('class', 'letraPalavra');
+        contador.setAttribute('id', 'letra' + i);
+        contador.setAttribute('value', arrPalavra[i]);
+        jogo.appendChild(contador);
+    }
+
+    letra.addEventListener('click', function (k) {
+        k.preventDefault(); //não envia formulário
+        let letraEsc = document.getElementById('letraE').value;
+        let errou = 0;
+
+        console.log(letraEsc);
+        console.log(arrPalavra);
+
+        for (let k = 0; k < arrPalavra.length; k++) {
+            if (letraEsc === arrPalavra[k]) {
+                document.getElementById('letra' + k).classList.toggle('letraPalavraVisivel')
+            } else if (letraEsc != arrPalavra[k]) {
+                errou += 1
+            }
+
+        }
+        if (errou === arrPalavra.length) {
+            errouFinal += 1
+        }
+        console.log(errou);
+
+        console.log(errouFinal);
+
+        if (errouFinal >= 7) {
+            alert("voce é burro ?")
+        }
+
+    })
+
+})
